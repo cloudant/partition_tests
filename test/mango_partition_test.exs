f6bfbee0242fe3ee4e4ae24ac49337f7dbe4bc6a
Garren Smith <garren.smith@gmail.com>
2018-09-10 11:51:30 +0200

    tests for global mango queries

--- test/mango_partition_test.exs
+++ test/mango_partition_test.exs
@@ -5,12 +5,15 @@ defmodule MangoPartitionTest do
   @moduledoc """
   Test Partition functionality for mango
   """
-  def create_index(db_name, fields \\ ["some"]) do
-    resp = Couch.post("/#{db_name}/_index", body: %{
+  def create_index(db_name, fields \\ ["some"], opts \\ %{}) do
+    default_index = %{
       index: %{
         fields: fields
       }
-    })
+    }
+
+    index = Enum.into(opts, default_index)
+    resp = Couch.post("/#{db_name}/_index", body: index)
 
     assert resp.status_code == 200
     assert resp.body["result"] == "created"
@@ -411,6 +414,86 @@ defmodule MangoPartitionTest do
     assert resp.status_code == 200
     partitions = get_partitions(resp)
     assert length(partitions) == 2
+    assert_correct_partition(partitions, "foo")
+  end
+
+  @tag :with_partitioned_db
+  test "partitioned database global query uses global index", context do
+    db_name = context[:db_name]
+    create_docs(db_name)
+    create_index(db_name, ["some"], %{partitioned: false})
+
+    url = "/#{db_name}/_explain"
+    selector = %{
+      selector: %{
+        some: "field"
+      }
+    }
+
+    resp = Couch.post(url, body: selector)
+    assert resp.status_code == 200
+    %{:body => body} = resp
+    assert body["index"]["def"] == %{"fields" => [%{"some" => "asc"}]}
+
+    url = "/#{db_name}/_find"
+    resp = Couch.post(url, body: selector)
+    assert resp.status_code == 200
+
+    partitions = get_partitions(resp)
+    assert length(partitions) == 100
+  end
+
+
+  @tag :with_partitioned_db
+  test "partitioned database global query does not use partition index", context do
+    db_name = context[:db_name]
+    create_docs(db_name)
+    create_index(db_name, ["some"])
+
+    url = "/#{db_name}/_explain"
+    selector = %{
+      selector: %{
+        some: "field"
+      }
+    }
+
+    resp = Couch.post(url, body: selector)
+    %{:body => body} = resp
+    assert body["index"]["name"] == "_all_docs"
+
+    url = "/#{db_name}/_find"
+    resp = Couch.post(url, body: selector)
+
+    assert resp.status_code == 200
+
+    partitions = get_partitions(resp)
+    assert length(partitions) == 100
+  end
+
+  @tag :with_partitioned_db
+  test "partitioned database partitioned query does not use global index", context do
+    db_name = context[:db_name]
+    create_docs(db_name)
+    create_index(db_name, ["some"], %{partitioned: false})
+
+    url = "/#{db_name}/_partition/foo/_explain"
+    selector = %{
+      selector: %{
+        some: "field"
+      }
+    }
+
+    resp = Couch.post(url, body: selector)
+    assert resp.status_code == 200
+    %{:body => body} = resp
+    assert body["index"]["name"] == "_all_docs"
+
+    url = "/#{db_name}/_partition/foo/_find"
+    resp = Couch.post(url, body: selector)
+    assert resp.status_code == 200
+
+    partitions = get_partitions(resp)
+    assert length(partitions) == 50
     assert_correct_partition(partitions, "foo")
   end
 
